515a6555fdefdce44b918798bd1f9e681ae9a6dc
shane <shanelianev@gmail.com>
2025-03-24 15:18:21 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,13 @@
 "use client";
 
 import "../styles/globals.css";
+import React from "react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head />
+      <body className="min-h-screen font-sans antialiased">{children}</body>
     </html>
   );
 }
